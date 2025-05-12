165e31656356189842e3307e1755ba2a4198401d
EtimbukUdofia <etimbukudofia9@gmail.com>
2025-05-12 11:53:06 +0100

    feat: add controler for player routes

--- server/routes/team.route.js
+++ server/routes/team.route.js
@@ -2,13 +2,14 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/");
-router.get("/:id");
+// router.get("/");
+// router.get("/:id");
+// router.get("/:id/players");
 
-router.post("/");
+// router.post("/");
 
-router.patch("/:id");
+// router.patch("/:id");
 
-router.delete("/:id");
+// router.delete("/:id");
 
 export default router;
